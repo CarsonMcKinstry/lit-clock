4e9c1022cf8b34548771ea33de9f5e0c0dd376d6
Carson McKinstry <carsonm@spotify.com>
2022-11-28 14:56:31 +0100

    fixed prisma

--- src/components/Quote/Quote.stories.tsx
+++ src/components/Quote/Quote.stories.tsx
@@ -1,8 +1,8 @@
-import { Quote as Component } from './Quote';
-import { DBQuote } from './types';
-import { ComponentMeta } from '@storybook/react';
+import {Quote as Component} from './Quote';
+import {DBQuote} from './types';
+import {ComponentMeta} from '@storybook/react';
 import './font.story.css';
-import { Provider as SettingsProvider } from '../Settings';
+import {Provider as SettingsProvider} from '../Settings';
 
 export default {
     title: 'Quote',
@@ -11,7 +11,7 @@ export default {
 
 const basicQuote: DBQuote = {
     time: '00:00',
-    time_string: 'midnight',
+    time_words: 'midnight',
     quote: 'As midnight was striking bronze blows upon the dusky air, Dorian Gray, dressed commonly, and with a muffler wrapped round his throat, crept quietly out of his house.',
     title: 'The Picture of Dorian Gray ',
     author: 'Oscar Wilde',
@@ -19,7 +19,7 @@ const basicQuote: DBQuote = {
 
 const longQuote: DBQuote = {
     time: '12:54',
-    time_string: '12:54 pm.',
+    time_words: '12:54 pm.',
     quote: "I listen to the different boats' horns, hoping to learn what kind of boat I'm hearing and what the signal means: is the boat leaving or entering the harbor; is it the ferry, or a whale-watching boat, or a fishing boat? At 5:33 pm there is a blast of two deep, resonant notes a major third apart. On another day there is the same blast at 12:54 pm. On another, exactly 8:00 am.",
     title: 'Varieties of Disturbance',
     author: 'Lydia Davis',
@@ -27,7 +27,7 @@ const longQuote: DBQuote = {
 
 const reallyLongQuote: DBQuote = {
     time: '20:15',
-    time_string: 'quarter past eight',
+    time_words: 'quarter past eight',
     quote: "Natsha: I was looking to see if there wasn't a fire. It's Shrovetide, and the servant is simply beside herself; I must look out that something doesn't happen. When I came through the dining-room yesterday midnight, there was a candle burning. I couldn't get her to tell me who had lighted it. [Puts down her candle] What's the time? Andrey: [Looks at his watch] A quarter past eight. Natasha: And Olga and Irina aren't in yet. The poor things are still at work. Olga at the teachers' council, Irina at the telegraph office...[sighs] I said to your sister this morning, \"Irina, darling, you must take care of yourself.\" But she pays no attention. Did you say it was a quarter past eight?",
     title: 'The Three Sisters',
     author: 'Anton Chekhov',
@@ -56,7 +56,8 @@ export const DarkMode = (args: any) => (
         <SettingsProvider
             value={{
                 dark_mode: true,
-                updateSettings: async () => {},
+                updateSettings: async () => {
+                },
             }}
         >
             <Component {...args} />
